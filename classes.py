68e87457cae41ebbb42cb10376941c71d79eb781
Vanda Hlaváčková <hlavackva@natur.cuni.cz>
2022-02-12 00:56:56 +0100

    procházení dat

--- classes.py
+++ classes.py
@@ -2,45 +2,40 @@ from math import sqrt
 
 #vytvoření třídy polyline
 class LineString():
-    def __init__(self, data = None): 
+    def __init__(self, coords): 
         self.linestring = []
-        self.data = data
-        if data is not None:
-            self.vstupni_data
         
-    #procházení dat
-    def vstupni_data (self):
-        segment_vstup = Segment()
-        for bod in self.data["geometry"]["coordinates"]:
-            point = Point(bod[0], bod[1])
-            if segment_vstup.bod1 is not None:
-                segment_vstup.bod2 = point
-                self.nove_segmenty(segment_vstup)
-            elif segment_vstup.bod1 is None:
-                segment_vstup.bod1 = point
+        #počet bodů v jednom LineString
+        count_points = (len(coords)-1)
 
-    #doplnění linestring o nově rozdělené segmenty
+        #procházení dat
+        for i in range(count_points):
+            point1 = Point(coords[i][0], coords[i][1])
+            point2 = Point(coords[i+1][0], coords[i+1][1])
+            segment = Segment(point1, point2)
+            self.linestring.append(segment)
+
+    """#doplnění linestring o nově rozdělené 
     def nove_segmenty (self, novy_segment):
         self.linestring.append(novy_segment) 
-    
+        return self.linestring"""
+
     #rozělení dlouhého segmentu
     def divide_long_segments(self, max_length):
-        new_linestring = LineString()
-        new_linestring.data = self.data 
+        new_linestring = []
         for line in self.linestring:
-            new_linestring = line.divide(max_length)
-            ###linestring_rozdeleni = line.divide(max_length)
-            self.linestring.append(new_linestring)
-        return new_linestring
-
+            new_line = line.divide(max_length)
+            new_linestring.extend(new_line)
+        self.linestring = new_linestring
+        
     #funkce pro zápis nových dat
     def zapis (self):
         coordinates = []
         for segment in self.linestring:
             coordinates.append([segment.bod1.x, segment.bod1.y])
-        coordinates.append([self.linestring[-1].bod2.x, self.linestring[-1].bod2.y])
-        self.data ["geometry"]["coordinates"] = coordinates
-        return self.data 
+        last_point = ([self.linestring[-1].bod2.x, self.linestring[-1].bod2.y])
+        coordinates.append(last_point)
+        return (coordinates)
 
 #vytvoření třídy segmentů
 class Segment():
@@ -50,28 +45,30 @@ class Segment():
 
     #výpočet délky segmentu
     def vzdalenost (self):
-        self.delka = sqrt((self.bod1.x - self.bod2.x)**2 + (self.bod1.y - self.bod2.y)**2)
-        return self.delka
-
-    #výpočet půlícího bodu v případě příliš dlouhého segmentu
+        delka = sqrt((self.bod1.x - self.bod2.x)**2 + (self.bod1.y - self.bod2.y)**2)
+        return delka
+    
+    """#půlící bod u dlouhého segmentu
     def middle_point (self):
-        middle_point = Point(((self.bod1.x + self.bod2.x)/2), ((self.bod1.y + self.bod2.y)/2))
-        return middle_point
-
+        self.middle = Point((self.bod1.x+self.bod2.x)/2, (self.bod1.y+self.bod2.y)/2)"""
+   
     #rozdělení příliš dlouhých segmentů
     def divide(self, max_length):
-        linestring = LineString()
-        if self.delka() >= max_length:
-            self.middle_point()
+        if self.vzdalenost() >= max_length:
+
+            #výpočet půlícího bodu v případě příliš dlouhého segmentu
+            middle_p = Point((self.bod1.x+self.bod2.x)/2, (self.bod1.y+self.bod2.y)/2)
 
             #vytváření nových segmentů
-            linestring.nove_segmenty(Segment(self.bod1, self.middle_point))
-            linestring.nove_segmenty(Segment(self.middle_point, self.bod2))
-            return linestring.divide_long_segments(max_length)
+            segment1= Segment(self.bod1, middle_p)
+            segment2= Segment(middle_p, self.bod2)
 
-        #vytvoření linestring z jednotlivých segmentů
-        linestring.nove_segmenty(self)
-        return linestring
+            #kontrola délky nových segmentů
+            new_line1 = segment1.divide(max_length)
+            new_line2 = segment2.divide(max_length)
+            return new_line1 + new_line2
+        
+        return [self]
 
 #vytvoření třídy bodů
 class Point():
